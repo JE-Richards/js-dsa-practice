412b762f03bef4c4281a1c267ef63b18aa68b0a0
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-08-10 12:45:12 +0100

    Implement node removal into binary search tree
    Added new methods to the binary search tree class:
    - minimum: returns the minimum value node in the tree
    - maximum: returns the maximum value node in the tree
    - remove: removes a given value node from the tree.
        - The remove function will ensure the principles of a binary search
        tree are retained when a node is removed.
    
    Additional changes:
    - Bug fix for BinarySearchTree search() method. The methods base case
    previously referred to 'currentNode.data === null' where it should have
    referred to 'currentNode === null', since if the node itself is null
    then node.data will throw a TypeError.

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -47,7 +47,7 @@ class BinarySearchTree {
 
   // private helper method to be called by search()
   #searchNode(startingNode, data) {
-    if (startingNode.data === null) {
+    if (startingNode === null) {
       return null;
     }
 
@@ -58,5 +58,75 @@ class BinarySearchTree {
     } else {
       return startingNode;
     }
+  }
+
+  // user accessible methods to find the minimum and maximum values in the tree
+  minimum() {
+    return this.#findMinNode(this.root);
+  }
+
+  maximum() {
+    return this.#findMaxNode(this.root);
+  }
+
+  // private method to find the minimum node in a tree - useful when removing nodes
+  #findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.#findMinNode(node.left);
+    }
+  }
+
+  // private method to find the maximum node in a tree
+  #findMaxNode(node) {
+    if (node.right === null) {
+      return node;
+    } else {
+      return this.#findMaxNode(node.right);
+    }
+  }
+
+  // user accessible method to remove a node
+  remove(data) {
+    this.root = this.#removeNode(this.root, data);
+  }
+
+  // private helper method to search if a node exists in the tree, remove it, and maintain structure of the tree
+  #removeNode(currentNode, data) {
+    // if tree is empty do nothing
+    if (currentNode === null) {
+      return null;
+    }
+
+    // Use recursion to traverse the tree to find if the node to remove
+    if (data < currentNode.data) {
+      currentNode.left = this.#removeNode(currentNode.left, data);
+    } else if (data > currentNode.data) {
+      currentNode.right = this.#removeNode(currentNode.right, data);
+    } else {
+      // else the node is found so we need to handle how the node is removed
+
+      // Case 1: Node is a leaf (has no children)
+      if (currentNode.left === null && currentNode.right === null) {
+        return null;
+      }
+
+      // Case 2: Node has a single child
+      if (currentNode.left === null) {
+        return currentNode.right; // replace the node with the right child
+      } else if (currentNode.right === null) {
+        return currentNode.left; // replace the node with the left child
+      }
+
+      // current node has 2 children - removal needs to ensure that the tree retains the properties of a binary search tree
+      // Start by finding the in-order successor (the smallest child node in the right subtree)
+      const minNode = this.#findMinNode(currentNode.right);
+      currentNode.data = minNode.data;
+
+      // Recursively remove the in-order successor
+      currentNode.right = this.#removeNode(currentNode.right, minNode.data);
+    }
+    return currentNode;
   }
 }
